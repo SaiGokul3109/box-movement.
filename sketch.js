285e5ac77780168c9d087c3dc06605dbcac7af6e
SaiGokul3109 <98755158+SaiGokul3109@users.noreply.github.com>
2022-02-18 19:23:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+var box1;
+var box2;
+
+function setup() {
+  createCanvas(600, 400);
+  box1=new Box();
+  box2=new Box();
+
+}
+
+function draw() {
+  background(220);
+box1.show();
+box1.set_speed(3);
+box2.show();
+box2.set_width(70);
+box2.set_speed(6);
+}
+
